a19ab85665e72a1e0d7b65ce4cfa9bb9918fc424
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-04-16 09:22:07 +0530

    [fix]: removeUser in userManager
    - the removeUser method shouldn't just filter the room users
    - rather it should filter and update the room users

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -35,11 +35,14 @@ export class UserManager {
 
     removeUser(roomId: string, userId: string) {
         console.log("removed user");
-        const users = this.rooms.get(roomId)?.users;
-        if (users) {
-            users.filter(({id}) => id !== userId);
+        const room = this.rooms.get(roomId);
+        if (room) {
+        room.users = room.users.filter(({ id }) => id !== userId);
+        console.log(`User with id ${userId} removed!`);
+        } else {
+        console.error(`Error occurred while removing user with id ${userId}`);
         }
-    }
+        }
     
     getUser(roomId: string, userId: string): User | null {
         const user = this.rooms.get(roomId)?.users.find((({id}) => id === userId));
